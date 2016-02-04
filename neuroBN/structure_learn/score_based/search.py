8fad2bc50d8771a52c5008a59cd0bd565f502155
ncullen93 <nicholas.c.cullen.th@dartmouth.edu>
2016-02-04 17:12:10 -0500

    score-based structure learning

--- neuroBN/structure_learn/score_based/search.py
+++ neuroBN/structure_learn/score_based/search.py
@@ -14,8 +14,55 @@ Local search - possible moves:
 
 from scipy.optimize import *
 
-def next_move():
-	pass
 
-def greedy_search():
-	pass
+def greedy(data, score='BIC', combined=False):
+	"""
+	Greedy search proceeds by choosing the move
+	which maximizes the increase in fitness of the
+	network at the current step. It continues until
+	it reaches a point where there does not exist any
+	feasible single move that increases the network fitness.
+
+	It is called "greedy" because it simply does what is
+	best at the current iteration only, and thus does not
+	look ahead to what may be better later on in the search.
+
+	The possible moves are the following:
+		- add edge
+		- delete edge
+		- invert edge
+
+	Arguments
+	---------
+	*data* : a nested numpy array
+		The data from which the Bayesian network
+		structure will be learned.
+
+	*score* : a string
+		Which score metric to use.
+		Options:
+			- AIC
+			- BIC / MDL
+			- LL (log-likelihood)
+	"""
+	nrow = data.shape[0]
+	ncol = data.shape[1]
+	
+	edge_dict = dict([(n,[]) for n in range(ncol)])
+
+	improvement = True
+
+	while improvement:
+		pass
+
+
+
+
+
+
+
+
+
+
+
+
