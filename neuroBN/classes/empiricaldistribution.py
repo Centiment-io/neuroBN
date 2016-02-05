852d9f89d2aa160980e65de556c10682ebd488dd
ncullen93 <nicholas.c.cullen.th@dartmouth.edu>
2016-02-04 20:28:21 -0500

    empirical distribution

--- neuroBN/classes/empiricaldistribution.py
+++ neuroBN/classes/empiricaldistribution.py
@@ -12,46 +12,91 @@ derived from a dataset.
 
 from __future__ import division
 
+import numpy as np
+
 
 class EmpiricalDistribution(object):
 
 
 	def __init__(self, data, names=None):
-		hist,_ = np.histogramdd(data, bins=bins)
-		self.joint = hist / hist.sum()
 
-		self.NROW = data.shape[0]
-		self.NVAR = data.shape[1]
 		if names is None:
 			self.names = range(data.shape[1])
 		else:
 			assert (len(names) == self.NVAR), 'Passed-in names length must equal number of data columns'
 			self.names = names
 
+		self.NROW = data.shape[0]
+		self.NVAR = data.shape[1]
+		self.bins = [len(np.unique(data[:,n])) for n in range(self.NVAR)]
+		
+
+		hist,_ = np.histogramdd(data, bins=self.bins)
+		self.joint = (hist / hist.sum()) + 1e-3
+
+		## COMPUTE MARGINAL FOR EACH VARIABLE ##
+		_range = range(self.NVAR)
+		for i,rv in enumerate(self.names()):
+			_axis = copy(_range)
+			_axis.remove(i)
+			self.marginal[rv] = np.sum(self.joint,axis=_axis)
+
 		self.marginal = dict([(rv, np.sum(self.joint,axis=i)) for i,rv in enumerate(self.names)])
 
 		self.cache = {}
 
-	def idx_map(self, **kwargs):
-		return tuple([self.names.index(kwarg) for kwarg in **kwargs])
+	def idx_map(self, rvs):
+		assert (isinstance(args, tuple)), "passed-in rvs must be a tuple"
+		idx = [self.names.index(rv) for rv in rvs]
+		return tuple(idx)
 
-	def jpd(self, **kwargs):
-		if **kwargs not in self.cache:
-			jpd = np.sum(self.joint, axis=self.idx_map(**kwargs))
-			self.cache[**kwargs] = jpd
+	def idx(self, rv):
+		return self.names.index(rv)
+
+	def axis_tuple(self, rv):
+		pass
+
+	def mpd(self, rv):
+		"""
+		Marginal Probability Distribtuion
+		"""
+		assert (isinstance(rv, str)), 'passed-in rv must be a string'
+		if rv in self.cache:
+			mpd = self.cache[rv]
 		else:
-			jpd = self.cache[**kwargs]
+			_axis = range(self.NVAR)
+			_axis.remove(self.idx(rv))
+			mpd = np.sum(self.join, axis=tuple(_axis)) # CORRECT
+			self.cache[rv] = mpd
+
+		return mpd
+
+	def jpd(self, rvs):
+		assert (isinstance(args, tuple)), "passed-in rvs must be a tuple"
+		if args in self.cache:
+			jpd = self.cache[rvs]
+		else:
+			jpd = np.sum(self.joint, axis=self.idx_map(rvs)) # WRONG 
+			self.cache[rvs] = jpd
+			
 		return jpd
 
-	def cpd(self, x, **kwargs):
-		_str = str(x) + '|' + ','.join(**Kwargs)
-		if _str in self.cache:
-			cpd self.cache[_str]
+	def cpd(self, lhs, rhs):
+		assert (isinstance(rhs, tuple)), "passed-in rhs must be a tuple"
+
+		_key = tuple((lhs,rhs))
+		if _key in self.cache:
+			cpd = self.cache[_key]
 		else:
-			tup = (x,) + **kwargs
-			_numer = self.jpd(tup)
-			_denom = self.jpd(**kwargs)
+			try:
+				tot = lhs + rhs
+			except TypeError:
+				tot = (lhs,) + rhs
+
+			_numer = self.jpd(tot)
+			_denom = self.jpd(rhs)
 			cpd = _numer / (_denom + 1e-7)
+			self.cache[_key] = cpd
 		
 		return cpd
 
