b776dbb0f8974f381e0bf7dd9335018439f88867
ncullen93 <nicholas.c.cullen.th@dartmouth.edu>
2016-02-10 09:57:14 -0500

    pygobn integration

--- neuroBN/structure_learn/exact/gobnilp.py
+++ neuroBN/structure_learn/exact/gobnilp.py
@@ -18,3 +18,14 @@ sample size, etc), along with various network constraints
 requirements).
 
 """
+__author__ = """Nicholas Cullen <ncullen.th@dartmouth.edu>"""
+
+def learn_gobnilp(data, settings=None, edge_reqs=None, nonedge_reqs=None, ind_reqs=None):
+	try:
+		from pyGOBN import *
+	except ImportError:
+		print 'Failed to import pyGOBN.'
+
+	gobn = GOBN()
+	gobn.set_settings(settings)
+	gobn.set_constraints(edge_reqs=edge_reqs, nonedge_reqs=nonedge_reqs, ind_reqs=ind_reqs)
